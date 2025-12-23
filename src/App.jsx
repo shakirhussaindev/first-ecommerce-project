c0bfc561059a3c3394288463169533d51493e2a9
shakirhussaindev <shakirhusain1414@gmail.com>
2025-12-23 13:34:24 +0600

    chore: setup ecommerce project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-black text-white text-center py-10 text-largg '>Ecommerce Project</h1>
+    </div>
+  )
+}
+
+export default App
